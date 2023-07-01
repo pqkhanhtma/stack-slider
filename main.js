71a3b23fb488254c34410029417053f230b9c66e
khanhphamqn <khanhphamqn@gmail.com>
2023-07-01 19:09:29 +0700

    update code

--- main.js
+++ main.js
@@ -1,25 +1,5 @@
 
 (function() {
-  // var rotate, timeline;
-
-  // nextRotate = function() {
-  //   return $('.card:first-child').fadeOut(400, 'swing', function() {
-  //     return $('.card:first-child').appendTo('.container').hide();
-  //   });
-  // };
-
-  // nextPrev = function() {
-  //   return $('.card:last-child').fadeOut(400, 'swing', function() {
-  //     return $('.card:last-child').prependTo('.container').hide();
-  //   });
-  // };
-
-  // $('.next').click(function() {
-  //     return nextRotate();
-  // });
-  // $('.prev').click(function() {
-  //   return nextPrev();
-  // });
   var slider = new StackSlider('.wrapper', {
     items: '.card'
   });
